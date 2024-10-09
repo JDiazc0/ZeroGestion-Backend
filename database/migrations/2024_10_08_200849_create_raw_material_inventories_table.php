e5fa853fe77ef172da94daeea4fae8c131c7ee9c
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-08 22:12:56 -0500

    feat: Create Inventory migrate

---
+++ database/migrations/2024_10_08_200849_create_raw_material_inventories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('raw_material_inventories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('raw_materials_id')->constrained('raw_materials');
+            $table->decimal('quantity', 10, 2);
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('raw_material_inventories');
+    }
+};
