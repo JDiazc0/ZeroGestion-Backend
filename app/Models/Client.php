0cb5ab404c211693e208533b8d49bc6d05b50596
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-08 22:11:47 -0500

    feat: Create Client migrate

---
+++ app/Models/Client.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function phones()
+    {
+        return $this->hasMany(ClientPhone::class);
+    }
+}
