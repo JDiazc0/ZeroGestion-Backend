b586f4282ed0e16671848138b0cda3c82732c878
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-08 22:08:52 -0500

    feat: Create Product migrate

---
+++ app/Models/Product.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'selling_price',
+        'production_cost',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the raw materials for the product.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function rawMaterials()
+    {
+        return $this->belongsToMany(RawMaterial::class, 'product_raw_materials')
+            ->using(ProductRawMaterial::class)
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
+
+    /**
+     * Get the orders for the product.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_products')
+            ->using(OrderProduct::class)
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
+
+
+    public function inventory()
+    {
+        return $this->hasOne(ProductInventory::class);
+    }
+}
