2507b42360614cd72004ac7061a902d36698dd87
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-08 22:12:29 -0500

    feat: Create Transaction migrate

---
+++ app/Models/Transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TransactionType;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'amount',
+        'user_id',
+    ];
+
+    protected $cast = [
+        'type' => TransactionType::class,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order()
+    {
+        return $this->hasOne(Order::class);
+    }
+}
---
+++ database/migrations/2024_10_08_145017_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\TransactionType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->decimal('amount', 10, 2);
+            $table->enum('type', TransactionType::values());
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('order_id')->constrained('orders')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
