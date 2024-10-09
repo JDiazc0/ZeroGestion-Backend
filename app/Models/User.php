41bce0f4dbd9b06ba97b26bf614149ab0d0b1b66
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-08 22:08:09 -0500

    feat: Create User migrate

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,34 +12,59 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'theme_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function theme()
+    {
+        return $this->belongsTo(Theme::class);
+    }
+
+    public function rawMaterials()
+    {
+        return $this->hasMany(RawMaterial::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function clients()
+    {
+        return $this->hasMany(Client::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function productInventory()
+    {
+        return $this->hasMany(ProductInventory::class);
+    }
+
+    public function rawMaterialInventory()
+    {
+        return $this->hasMany(RawMaterialInventory::class);
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('password');
+            $table->foreignId('theme_id')->constrained('themes');
             $table->rememberToken();
             $table->timestamps();
         });
