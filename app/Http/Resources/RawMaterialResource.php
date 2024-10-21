673003bc394a66efede44daf5d298bba7a3ceb51
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-21 15:10:43 -0500

    feat (Resoruce): Create Raw material resource

---
+++ app/Http/Resources/RawMaterialResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RawMaterialResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'min_quantity' => $this->min_quantity,
+            'measure' => $this->measure,
+        ];
+    }
+}
