f68607e77b0090a17187c6890795a3ed89e6eb70
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-21 15:13:56 -0500

    feat (routes): Create api routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RawMaterialController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Public routes
+Route::post('register', [AuthController::class, 'registerUser']);
+Route::post('login', [AuthController::class, 'loginUser']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', [AuthController::class, 'logoutUser']);
+
+    // Protected raw material routes
+    Route::group(['prefix' => 'raw-material'], function () {
+        Route::post('create', [RawMaterialController::class, 'store']);
+        Route::get('index', [RawMaterialController::class, 'index']);
+    });
 });
