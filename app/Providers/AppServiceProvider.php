4c355351a356d654717e6b6a85d65484c6cc49fd
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-21 15:09:45 -0500

    feat (ServiceProvider): Add Raw Material services as singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\RawMaterialService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Registers raw material as singleton.
+        $this->app->singleton(RawMaterialService::class, function ($app) {
+            return new RawMaterialService();
+        });
     }
 
     /**
