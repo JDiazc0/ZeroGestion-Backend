087be5877cc197ab7cddeed515cce33d944c5b70
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-21 15:12:51 -0500

    feat (Request): Create request for store raw materials

---
+++ app/Http/Requests/StoreRawMaterialRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\MeasureType;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRawMaterialRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'cost' => 'required|numeric|min:0',
+            'min_quantity' => "required|numeric|min:0",
+            'measure' => "required|string|in:" . implode(",", MeasureType::values()),
+        ];
+    }
+
+    /**
+     * Get custom error messages for validation
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'measure.in' => 'The selected measure is invalid. Valid options are: ' . implode(', ', MeasureType::values()),
+        ];
+    }
+}
