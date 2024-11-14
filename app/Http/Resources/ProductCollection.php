5217b77dd45bb23c2688c2d8ceddda68f21484a4
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-14 14:45:55 -0500

    Feat (Resources) Create colletion resource for products

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'products' => $this->collection->map(function ($product) {
+                return [
+                    'name' => $product->name,
+                    'selling_price' => $product->selling_price,
+                    'inventory' => [
+                        'id' => $product->inventory->id,
+                        'quantity' => $product->inventory->quantity,
+                    ],
+                    'raw_materials' => $product->rawMaterials ? $product->rawMaterials->map(function ($raw_material) {
+                        return [
+                            'id' => $raw_material->id,
+                            'name' => $raw_material->name,
+                            'quantity' => $raw_material->pivot->quantity,
+                        ];
+                    }) : [],
+                ];
+            }),
+            'total' => $this->collection->count()
+        ];
+    }
+}
