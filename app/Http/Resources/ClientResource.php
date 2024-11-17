154b2989f62ad019166a09088b568c5fc566b0f7
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-17 16:36:12 -0500

    feat (Resource) Client resource

---
+++ app/Http/Resources/ClientResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'address' => $this->address,
+            'phones' => $this->when($this->relationLoaded('phones'), function () {
+                return $this->phones->map(function ($phones) {
+                    return [
+                        'phone_number' => $phones->phone_number,
+                    ];
+                });
+            })
+        ];
+    }
+}
