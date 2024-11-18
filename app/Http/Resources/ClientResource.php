dbc859aa3911d783a3993ab84b8d1595a34dafbe
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-18 10:58:52 -0500

    feat: add id in resource

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -15,11 +15,13 @@ class ClientResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'address' => $this->address,
             'phones' => $this->when($this->relationLoaded('phones'), function () {
                 return $this->phones->map(function ($phones) {
                     return [
+                        'id' => $phones->id,
                         'phone_number' => $phones->phone_number,
                     ];
                 });
