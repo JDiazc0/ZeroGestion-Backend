d7712450c423259e8d090cd4926506c3b7fd7727
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-18 10:58:34 -0500

    feat(Collection) Create client collection

---
+++ app/Http/Resources/ClientCollecton.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ClientCollecton extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'clients' => $this->collection->map(function ($client) {
+                return [
+                    'id' => $client->id,
+                    'name' => $client->name,
+                    'address' => $client->address,
+                    'phones' => $client->phones ? $client->phones->map(function ($phone) {
+                        return [
+                            'phone_number' => $phone->phone_number
+                        ];
+                    }) : []
+                ];
+            }),
+            'total' => $this->collection->count()
+        ];
+    }
+}
