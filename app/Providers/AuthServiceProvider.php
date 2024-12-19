041071b4f19bb4b64641eba912383c67c795ed0f
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-12-19 18:01:35 -0500

    feat create inventories policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,8 +6,12 @@ namespace App\Providers;
 
 use App\Models\Client;
 use App\Models\Product;
+use App\Models\ProductInventory;
 use App\Models\RawMaterial;
+use App\Models\RawMaterialInventory;
 use App\Policies\ClientPolicy;
+use App\Policies\InventoryProductPolicy;
+use App\Policies\InventoryRawMaterialPolicy;
 use App\Policies\ProductPolicy;
 use App\Policies\RawMaterialPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -23,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         RawMaterial::class => RawMaterialPolicy::class,
         Product::class => ProductPolicy::class,
         Client::class => ClientPolicy::class,
+        ProductInventory::class => InventoryProductPolicy::class,
+        RawMaterialInventory::class => InventoryRawMaterialPolicy::class,
     ];
 
     /**
