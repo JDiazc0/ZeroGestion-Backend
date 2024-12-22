594e20e737a4860abbe4cdaf148689a58ed362af
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-12-22 16:46:46 -0500

    feat create policies

--- app/Policies/InventoryProductPolicy.php
+++ app/Policies/InventoryProductPolicy.php
@@ -2,15 +2,33 @@
 
 namespace App\Policies;
 
+use App\Models\ProductInventory;
+use App\Models\RawMaterialInventory;
 use App\Models\User;
 
 class InventoryProductPolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view the model.
      */
-    public function __construct()
+    public function view(User $user, ProductInventory $productInventory): bool
     {
-        //
+        return $user->id === $productInventory->user_id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, ProductInventory $productInventory): bool
+    {
+        return $user->id === $productInventory->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, ProductInventory $productInventory): bool
+    {
+        return $user->id === $productInventory->user_id;
     }
 }
--- app/Policies/InventoryRawMaterialPolicy.php
+++ app/Policies/InventoryRawMaterialPolicy.php
@@ -2,15 +2,32 @@
 
 namespace App\Policies;
 
+use App\Models\RawMaterialInventory;
 use App\Models\User;
 
 class InventoryRawMaterialPolicy
 {
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view the model.
      */
-    public function __construct()
+    public function view(User $user, RawMaterialInventory $rawMaterialInventory): bool
     {
-        //
+        return $user->id === $rawMaterialInventory->user_id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, RawMaterialInventory $rawMaterialInventory): bool
+    {
+        return $user->id === $rawMaterialInventory->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, RawMaterialInventory $rawMaterialInventory): bool
+    {
+        return $user->id === $rawMaterialInventory->user_id;
     }
 }
